3dd12e32f59901dcb057f3787a314e76e5d75d70
bbramanti <bbramanti@gmail.com>
2019-06-15 17:35:21 -0500

    fixed indents

--- rotate-matrix.py
+++ rotate-matrix.py
@@ -3,9 +3,9 @@ import numpy as np
 def rotate(mat,degrees):
 	# a dictionary allows conversion
 	# from degrees --> # of counter-clockwise rotations
-    dict={90:3,180:2,270:1}
-    # rot90() rotates counter-clockwise
-    print (np.rot90(mat,dict[degrees]))
+	dict={90:3,180:2,270:1}
+	# rot90() rotates counter-clockwise
+	print (np.rot90(mat,dict[degrees]))
 
 mat1 = [[1,2,3],
         [4,5,6],
