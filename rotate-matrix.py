869eb7d0cd1c937837ab3992dfb1dbbf90f62684
bbramanti <bbramanti@gmail.com>
2019-06-15 17:15:22 -0500

    adding rotate-matrix.py

---
+++ rotate-matrix.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def rotate(mat,degrees):
+	# a dictionary allows conversion
+	# from degrees --> # of counter-clockwise rotations
+    dict={90:3,180:2,270:1}
+    # rot90() rotates counter-clockwise
+    print (np.rot90(mat,dict[degrees]))
+
+mat1 = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+#rotate a matrix 270 degrees clockwise
+rotate(mat1,270)
+#rotate a matrix 180 degrees clockwise                
+rotate(mat1,180)
+#rotate a matrix 90 degrees clockwise
+rotate (mat1, 90)
